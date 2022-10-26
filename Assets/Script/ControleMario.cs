1364699430c2112066b0eafa9b400dc6cb23d463
Galaxiio <83291094+Galaxiio@users.noreply.github.com>
2022-10-26 17:26:32 +0200

    ANIMATION V2

--- Assets/Script/ControleMario.cs
+++ Assets/Script/ControleMario.cs
@@ -4,27 +4,44 @@ using UnityEngine;
 
 public class ControleMario : MonoBehaviour
 {
-    private Rigidbody rb;
+    private Animator ani;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
+        ani = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float vitesse = 0;
 
+        if(ani.GetBool("isFalling"))
+        {
+            ani.SetBool("isFalling", false);
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            vitesse = 1;
+        }
 
         if(Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.Rotate(0, 2*Time.deltaTime*100.0f, 0);
+            this.transform.Rotate(0, 2*Time.deltaTime*75.0f, 0);
         }
 
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            this.transform.Rotate(0, -2*Time.deltaTime*100.0f, 0);
+            this.transform.Rotate(0, -2*Time.deltaTime*75.0f, 0);
         }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            ani.SetBool("isFalling", true);
+        }
+
+        ani.SetFloat("Speed", vitesse);
     }
 }
